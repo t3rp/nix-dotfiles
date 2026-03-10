597d50e4462a45d0011d0f771ef522675449204a
t3rp <190659213+t3rp@users.noreply.github.com>
2026-03-10 13:30:42 -0400

    Updated flake and flake.lock

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       # This ensures home-manager uses the same nixpkgs as our flake
       # Prevents version mismatches and reduces disk usage
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,7 +16,9 @@
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
       homeConfigurations = {
