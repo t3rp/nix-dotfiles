970cfaa6a47dd17e379f8cb17254cd3baa0725f8
t3rp <190659213+t3rp@users.noreply.github.com>
2026-03-13 08:30:21 -0400

    Fixed ssh agent with yubikey

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,54 +1,22 @@
 { pkgs, ... }:
 
 {
-	home.packages = with pkgs; [
-		openssh
-		yubikey-manager
-	];
-
-	# Use a dedicated OpenSSH agent socket instead of the GNOME/GCR agent.
-	home.sessionVariables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/ssh-agent.socket";
-
-	systemd.user.sockets.ssh-agent = {
-		Unit = {
-			Description = "OpenSSH agent socket";
-		};
-		Socket = {
-			ListenStream = "%t/ssh-agent.socket";
-			SocketMode = "0600";
-		};
-		Install = {
-			WantedBy = [ "sockets.target" ];
-		};
-	};
-
-	systemd.user.services.ssh-agent = {
-		Unit = {
-			Description = "OpenSSH agent";
-		};
-		Service = {
-			Type = "simple";
-			Environment = [ "SSH_AUTH_SOCK=%t/ssh-agent.socket" ];
-			ExecStart = "${pkgs.openssh}/bin/ssh-agent -D -a %t/ssh-agent.socket";
-		};
-		Install = {
-			WantedBy = [ "default.target" ];
-		};
-	};
-
 	programs.ssh = {
 		enable = true;
+		enableDefaultConfig = false;
+		matchBlocks = {
+			"*" = {
+				addKeysToAgent = "yes";
+				identitiesOnly = true;
+				identityAgent = "$SSH_AUTH_SOCK";
+			};
 
-		extraConfig = ''
-			Host *
-				IdentityAgent ${"$SSH_AUTH_SOCK"}
-				AddKeysToAgent yes
-				IdentitiesOnly yes
-
-			Host github.com
-				HostName github.com
-				User git
-				IdentityFile ~/.ssh/id_ed25519_sk_rk
-		'';
+			"github.com" = {
+				hostname = "github.com";
+				user = "git";
+				identitiesOnly = true;
+				identityFile = [ "~/.ssh/id_ed25519_sk_rk" ];
+			};
+		};
 	};
 }
